0249b60d605157ef49a2c0e7152e0190785bc9c6
vital999 <roma.ne.roma28@gmail.com>
2024-05-24 13:13:13 +0300

    upd

--- src/LAB_4.java
+++ src/LAB_4.java
@@ -1,15 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class LAB_4 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] evenArray = new int[50];
+        for (int i = 0; i < evenArray.length; i++) {
+            evenArray[i] = 2 * i;
         }
+
+
+        int[] oddArray = new int[50];
+        for (int i = 0; i < oddArray.length; i++) {
+            oddArray[i] = 2 * i + 1;
+        }
+
+
+        int[] numbers = {2, 17, 13, 6, 22, 31, 45, 66, 100, -18};
+        int i = 0;
+        while (i < numbers.length) {
+            System.out.print(numbers[i] + " ");
+            i++;
+        }
+        System.out.println();
+
+
+        for (int j = 0; j < numbers.length; j++) {
+            System.out.print(numbers[j] + " ");
+        }
+        System.out.println();
+
+
+        i = 0;
+        System.out.print("Елементи з непарними індексами: ");
+        while (i < numbers.length) {
+            if (i % 2 != 0) {
+                System.out.print(numbers[i] + " ");
+            }
+            i++;
+        }
+        System.out.println();
+
+
+        System.out.print("Елементи з парним індексом: ");
+        for (int j = 0; j < numbers.length; j++) {
+            if (j % 2 == 0) {
+                System.out.print(numbers[j] + " ");
+            }
+        }
+        System.out.println();
+
+
+        System.out.print("Обернений масив: ");
+        for (int j = numbers.length - 1; j >= 0; j--) {
+            System.out.print(numbers[j] + " ");
+        }
+        System.out.println();
+
+
+        double[] realNumbers = {1.2, 2.3, 3.4, 4.5, 5.6};
+        double sum = 0;
+        for (int j = 0; j < realNumbers.length; j++) {
+            sum += realNumbers[j];
+        }
+        System.out.println("Сума дійсних чисел: " + sum);
+
+
+        int[] integers = {-2, -17, 13, -6, 22, -31, 45, -66, 100, 18};
+        for (int j = 0; j < integers.length; j++) {
+            if (integers[j] % 2 != 0) {
+                integers[j] = -integers[j];
+            }
+        }
+        System.out.print("Цілі числа зі зміненими знаками: ");
+        for (int j = 0; j < integers.length; j++) {
+            System.out.print(integers[j] + " ");
+        }
+        System.out.println();
     }
 }
